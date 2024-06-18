c85290ce2f4deda9e58c6830a70991c9e15b5b7d
riddhidomadiyaa <test01.rentechdigital@gmail.com>
2024-06-18 12:31:55 +0530

    changes

--- index.js
+++ index.js
@@ -1,33 +1,12 @@
 const express = require("express")
 const app = express()
 app.use(express.json())
-const port = 5002
+const port = process.env.PORT || 80
 require("./db/db")
-const multer = require("multer")
-const path = require("path");
-const user = require('./route/users')
 
-app.use("/user", user)
-
-
-const storage = multer.diskStorage({
-	destination: function (req, file, cb) {
-		cb(null, 'uploads');
-	},
-	filename: function (req, file, cb) {
-		cb(null, file.fieldname + '_' + Date.now() + path.extname(file.originalname));
-	}
+app.get('/', async (req, res) => {
+	res.send("backend api run on server...");
 });
-
-const upload = multer({ storage: storage });
-
-app.post('/upload', upload.array('file', 12), async (req, res) => {
-	res.send("img upload")
-})
-// app.post('/upload',upload.single('file'), async (req, res) => {
-// 	res.send("img upload")
-// })
-
 
 app.listen(port, () => {
     console.log(`server started on port ${port}`);
